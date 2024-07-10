309b69df679d7ef2764fa9c1a4932a80fff919cf
Venkata <gangaa@gmail.com>
2024-07-10 09:58:56 +0530

    initial commit

---
+++ src/main/java/com/vk/webclient/imperative/wci/WciApplication.java
@@ -0,0 +1,13 @@
+package com.vk.webclient.imperative.wci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WciApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WciApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vk/webclient/imperative/wci/WciApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vk.webclient.imperative.wci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WciApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
